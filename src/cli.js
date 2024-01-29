51c920154fef830c65a8427335f49082cb85ba91
FinderHome <uoa-bm@yandex.ru>
2024-01-29 21:07:06 +0300

    correction of spelling errors

--- src/cli.js
+++ src/cli.js
@@ -1,9 +1,8 @@
 import readlineSync from 'readline-sync';
 
-
 const getNameGreet = () => {
-    const name = readlineSync.question('May I have your name? ');
-    return `Hello, ${name}!`;
+  const name = readlineSync.question('May I have your name? ');
+  return `Hello, ${name}!`;
 };
 
 export default getNameGreet;
