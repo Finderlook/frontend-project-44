fd4a9677d258db51d63ace796bcdfb8c70e93be8
FinderWork <uoa-bm@yandex.ru>
2024-02-14 09:37:17 +0300

    work on bugs

--- src/games/calc.js
+++ src/games/calc.js
@@ -2,12 +2,29 @@ import { getRandomInt, runLogic } from '../game-engine.js';
 
 const description = 'What is the result of the expression?';
 
-// const logicGames = () => {
-//   const randomInt = getRandomInt(0, 100);
-//   const question = String(randomInt);
-//   const answer = (randomInt % 2 === 0) ? 'yes' : 'no';
-//   return [question, answer];
-// };
+const operators = ['+', '-', '*'];
+
+const calculate = (num1, num2, operator) => {
+  if (operator === '+') {
+    return num1 + num2;
+  }
+  if (operator === '-') {
+    return num1 - num2;
+  }
+  if (operator === '*') {
+    return num1 * num2;
+  }
+  return console.log('There is no such operator');
+};
+
+const logicGames = () => {
+  const randomInt1 = getRandomInt(0, 25);
+  const randomInt2 = getRandomInt(0, 25);
+  const randomOperator = getRandomInt(0, operators.length - 1);
+  const question = `${randomInt1} ${operators[randomOperator]} ${randomInt2}`;
+  const answer = calculate(randomInt1, randomInt2, operators[randomOperator]);
+  return [question, String(answer)];
+};
 
 const brainCalcGame = () => runLogic(description, logicGames);
 export default brainCalcGame;
