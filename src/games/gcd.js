c607cfa60650272bcd59721aac9084220ebd3cb1
FinderWork <uoa-bm@yandex.ru>
2024-02-14 16:06:48 +0300

    add new game brain-gcd

---
+++ src/games/gcd.js
@@ -0,0 +1,25 @@
+import { getRandomInt, runLogic } from '../game-engine.js';
+
+const description = 'Find the greatest common divisor of given numbers.';
+
+const gcd = (num1, num2) => {
+  let a = num1;
+  let b = num2;
+  while (b !== 0) {
+    const tmp = a % b;
+    a = b;
+    b = tmp;
+  }
+  return a;
+};
+
+const logicGames = () => {
+  const randomInt1 = getRandomInt(0, 100);
+  const randomInt2 = getRandomInt(0, 100);
+  const question = `${randomInt1} ${randomInt2}`;
+  const answer = gcd(randomInt1, randomInt2);
+  return [question, String(answer)];
+};
+
+const brainGcdGame = () => runLogic(description, logicGames);
+export default brainGcdGame;
